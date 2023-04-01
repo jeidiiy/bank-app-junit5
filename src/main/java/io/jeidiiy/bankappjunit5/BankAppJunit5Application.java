a9f1d16002dc8d6d6a8a03734b681ceff164a333
Jeidiiy <jeidiiy98@gmail.com>
2023-04-01 22:15:05 +0900

    Initial Commit

---
+++ src/main/java/io/jeidiiy/bankappjunit5/BankAppJunit5Application.java
@@ -0,0 +1,13 @@
+package io.jeidiiy.bankappjunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppJunit5Application.class, args);
+	}
+
+}
