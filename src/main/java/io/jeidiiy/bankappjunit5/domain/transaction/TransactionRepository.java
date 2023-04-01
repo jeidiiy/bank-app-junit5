a0f8c8fed1e92810282a51c7f4d1159043dbf7da
Jeidiiy <jeidiiy98@gmail.com>
2023-04-02 00:21:49 +0900

    feat: 엔티티 별 레포지토리 생성

---
+++ src/main/java/io/jeidiiy/bankappjunit5/domain/transaction/TransactionRepository.java
@@ -0,0 +1,6 @@
+package io.jeidiiy.bankappjunit5.domain.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
---
+++ src/main/java/io/jeidiiy/bankappjunit5/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package io.jeidiiy.bankappjunit5.domain.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+}
